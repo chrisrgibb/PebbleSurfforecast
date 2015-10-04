9eba8b32b3b85a701bcdf7534a44e8cbb4c86d7b
Chris Gibb <chrisrgibb@gmail.com>
2015-10-04 21:26:09 +1300

    update app to take multiple surf spots and add in wind direction

--- app.js
+++ app.js
@@ -4,47 +4,93 @@
  * This is where you write your app.
  */
 
+var KEY = "Put your api key here";
+
+var spots = {
+  "Lyall Bay" :  "-41.3284420,174.7972330",
+  "Titahi Bay" : "-41.1061560,174.8386830",
+  "CastlePoint" : "-40.908919,176.222706"
+};
+
 var UI = require('ui');
 var ajax = require('ajax');
 
-// Create a Card with title and subtitle
+
 var card = new UI.Card({
   title:'Surf Forecast',
   subtitle:'Fetching...'
 });
 
-// Display the Card
-card.show();
 
+var getMainMenu = function(){
+  var menuItems = [];
 
-var lyall = "-41.3284420,174.7972330";
-var KEY = "Put your api key here";
-var URL = 'http://api.worldweatheronline.com/free/v2/marine.ashx?q=' + lyall + "&key=" +KEY + "&format=json";
-
-
-//Make the request
-ajax(
-  {
-    url: URL,
-    type: 'json'
-  },
-  function(response) {
-    // Success!
-    console.log('Successfully fetched surf data!');
-    
-    var firstHour = response.data.weather[0].hourly[0];
-    
-    var height = firstHour.sigHeight_m;
-    var period = firstHour.swellPeriod_secs;
-    var location = "Lyall Bay";
-    
-    card.subtitle(location + ', ');
-    card.body("height : " + height + "m.\n period : " + period + " secs");
-  },
-  function(error) {
-    // Failure!
-    console.log('Failed fetching surf data: ' + error);
+  for(var key in spots){
+    menuItems.push({
+      title : key
+    });  
   }
-);
+  
+  var mainMenu = new UI.Menu({
+    sections : [{
+      title : 'Get Forecast',
+      items : menuItems
+    }]
+  });
+  return mainMenu;
+};
+
+var mainMenu = getMainMenu();
+
+
+var doAjax = function(spotName){
+  var latlon = spots[spotName];
+  
+  var URL = 'http://api.worldweatheronline.com/free/v2/marine.ashx?q=' + latlon + "&key=" +KEY + "&format=json";
+  
+  card.show();
+  mainMenu.hide();
+  
+  ajax(
+    {
+      url: URL,
+      type: 'json'
+    },
+    function(response) {
+      // Success!
+      console.log('Successfully fetched surf data!');
+
+      var firstHour = response.data.weather[0].hourly[0];
+
+      var height = firstHour.sigHeight_m;
+      var period = firstHour.swellPeriod_secs;
+      var winddir = firstHour.winddir16Point;
+
+      card.subtitle(spotName + ', ');
+      card.body("height : " + height + "m.\nperiod : " + period + " secs\nwind : " + winddir);
+
+
+    },
+    function(error) {
+      // Failure!
+      console.log('Failed fetching surf data: ' + error);
+    }
+  );
+};
+
+mainMenu.on('select', function(e){
+  doAjax(e.item.title);
+});
+
+card.on('click', function(e){
+  mainMenu.show();
+  card.hide();
+  card.subtitle('Fetching...');
+  card.body('');
+});
+
+// start of app
+mainMenu.show();
+
 
 
